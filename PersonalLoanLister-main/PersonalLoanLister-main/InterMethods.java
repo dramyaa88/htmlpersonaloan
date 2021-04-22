cd403a178dda006ad209af6ccd70fb5a9ed637d1
dramyaa88 <58719475+dramyaa88@users.noreply.github.com>
2021-04-22 17:52:48 +0530

    Update InterMethods.java

--- PersonalLoanLister-main/PersonalLoanLister-main/InterMethods.java
+++ PersonalLoanLister-main/PersonalLoanLister-main/InterMethods.java
@@ -5,5 +5,13 @@ public interface InterMethods {
 	public abstract void eligibility(String name, String city, String phNumber, String pan, int age, int sal, int expense);
 	public abstract void loanDetails(int loanAmount,int tenure);
 	public abstract void endMessage();
+	public abstract void newUserDetails();
+	public abstract void existingUserDetails();
+	public abstract void loanChange();
+	public abstract void insertDisplay();
+	public abstract void display();
+	public abstract void deleteInfo();
+	public abstract void validate();
+	public abstract void updateInfo();
 
 }
